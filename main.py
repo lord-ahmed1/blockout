3b1b12dc9e36eff85e6a5b5eba8978f2f6df976b
lord-ahmed1 <105877811+lord-ahmed1@users.noreply.github.com>
2024-05-16 14:52:43 +0300

    Update main.py solved problem not detecting options button

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ pssd=getpass('Password:')
 
 thisDir=os.getcwd()
 print(thisDir)
-"/_internal/geckodriver.exe"
+                                        # change driver path according to where you place geckodriver "/_internal/geckodriver.exe"
 ff=wd.Firefox(executable_path=thisDir+"/_internal/geckodriver.exe")
 ff.get('https://www.instagram.com/')
 sleep(20)
@@ -26,17 +26,14 @@ ff.find_element(By.XPATH,'//input[@name="username"]').send_keys(username)
 ff.find_element(By.XPATH,'//input[@name="password"]').send_keys(pssd)
 ff.find_element(By.XPATH,'//button[@type="submit"]').click()
 sleep(10)
-ff.find_element(By.XPATH,'//div[contains(text(),"Not now")]').click()
-sleep(8)
-ff.find_element(By.XPATH,'//*[contains(text(),"Not Now")]').click()
-sleep(10)
+
 
 successful=0
 def instgram_block(target,successful):
     ff.get(target)
-    sleep(10)
-    ff.find_element(By.XPATH,'//div[@class="x6s0dn4 x78zum5 xdt5ytf xl56j7k"]').click()
-    sleep(10)
+    sleep(7)
+    ff.find_element(By.XPATH,'//div[@class="x1q0g3np x2lah0s"]').click()
+    sleep(7)
     block_button=ff.find_element(By.XPATH,'//button[@class="_a9-- _ap36 _a9-_"]')
     check=(block_button.text)
     if check !="Unblock":
@@ -45,27 +42,29 @@ def instgram_block(target,successful):
         ff.find_element(By.XPATH,'//div[@class="x78zum5 xdt5ytf x1crbq5u xvrdyt3 x179zr98"]//button').click()
         print('blocked !')
         successful=successful+1
-        print('successful blocks :{successful}')
+        print(f'successful blocks :{successful}')
     else:
         print('already blocked')
     return successful
+
 
 
 linksurl='https://docs.google.com/spreadsheets/d/1_BOYkvDgXGIjcAl1ZdYNuxN6gV-lARKGW9TOhHPVAh4/htmlview'
 site=get(linksurl).content
 table_rows=BeautifulSoup(site).find('table',{"class":"waffle"}).find_all('tr')
 for  index,row in enumerate(table_rows):
-    print(f'finished {index/len(table_rows)*100}')
+    print(f'finished {int(index/len(table_rows)*100)} %')
     
     try:
         separate_data=(row.find_all('td'))
         name=separate_data[0].text
         instgram=separate_data[1].text
         print(name,instgram)
-        successful=instgram_block(instgram)
+        successful=instgram_block(instgram,successful)
     except:
         pass
 ff.close()
 quit()
+
 
 
